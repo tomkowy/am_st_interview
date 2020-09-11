3ba055d2ede825ac5fe2400a61f75415cea9d74a
TomekkM <o2.tomek@tlen.pl>
2020-09-11 11:57:52 +0200

    add web application

---
+++ AmStInter/AmStInter.WebApp/Data/OrdersService.cs
@@ -0,0 +1,36 @@
+﻿using AmStInter.Application.Orders.Commands.UpdateStock;
+using AmStInter.Application.Orders.Queries.GetInProgressOrders;
+using AmStInter.Application.Orders.Queries.GetTopSoldProducts;
+using MediatR;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AmStInter.WebApp.Data
+{
+    public class OrdersService
+    {
+        private readonly IMediator _mediator;
+
+        public OrdersService(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task<IEnumerable<InProgressOrderVM>> GetOrders()
+        {
+            var orders = await _mediator.Send(new GetInProgressOrdersQuery());
+            return orders;
+        }
+
+        public async Task<IEnumerable<TopSoldProductVM>> GetTopSoldProducts()
+        {
+            var products = await _mediator.Send(new GetTopSoldProductsQuery());
+            return products;
+        }
+
+        public async Task UpdateProductStock(string merchantNumber)
+        {
+            await _mediator.Send(new UpdateStockCommand(merchantNumber));
+        }
+    }
+}
